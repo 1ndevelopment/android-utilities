6deb003d382c3677e37f515f31168c8410a7d350
1ndev-ui <1ndevelopment@protonmail.com>
2024-07-19 01:43:18 -0700

    super.img -> super.tar.gz

--- stock-ota-flasher-template/package_ota.sh
+++ stock-ota-flasher-template/package_ota.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-# Check if 7z is installed
-if ! command -v 7z &> /dev/null; then
-    echo "7-Zip is not installed. Installing..."
-    # Update package list and install p7zip
-    sudo apt update && sudo apt install -y p7zip-full
-    if [ $? -ne 0 ]; then
-        echo "Failed to install 7-Zip. Exiting."
-        exit 1
-    fi
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check for 7z and pigz
+if ! command_exists 7z; then
+    echo "7z is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y p7zip-full
 else
-    echo "7-Zip is installed."
+    echo "7z is already installed."
 fi
 
+if ! command_exists pigz; then
+    echo "pigz is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y pigz
+else
+    echo "pigz is already installed."
+fi
+echo ''
+echo "Both 7z and pigz are installed. Proceeding with further commands..."
+
+
+echo ''
+echo "Compressing super.img..."
+tar --use-compress-program="pigz -9 -p$(nproc)" -cf ./Firmware/super.tar.gz ./Firmware/super.img
+mv ./Firmware/super.img ./
+
+echo ''
 echo "Archiving OTA .zip..."
-7z a -tzip stock-ota.zip bin Firmware image META-INF
+7z a -tzip stock-ota.zip bin Firmware META-INF
+
+echo ''
+echo "Cleaning up..."
+rm -r ./Firmware/super.tar.gz
+mv ./super.img ./Firmware/
