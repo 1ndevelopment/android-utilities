6c79e61e75830fa58d7a90eba8a823ed0c91e0ed
1ndev <85550685+1ndev-ui@users.noreply.github.com>
2024-08-21 23:36:15 -0700

    Added the 'biggest' alias to zshrc

--- scripts/termux_scripts/ohmyzsh_termux_install.sh
+++ scripts/termux_scripts/ohmyzsh_termux_install.sh
@@ -2,8 +2,9 @@ pkg install zsh lsd git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-echo "alias ls=lsd" >> ~/.zshrc
-echo "alias speedtest=speedtest-go" >> ~/.zshrc
+echo "alias ls='lsd'" >> ~/.zshrc
+echo "alias speedtest='speedtest-go'" >> ~/.zshrc
+echo "alias biggest='du . -ah | sort -hr | head -n $1'" >> ~/.zshrc
 git clone https://github.com/adi1090x/termux-style
 cd termux-style
 ./install
