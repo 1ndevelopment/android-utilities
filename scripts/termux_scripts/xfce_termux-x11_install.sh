5571287576c9f38ad56ab666063cc30fc7ffd1fd
1ndev <85550685+1ndev-ui@users.noreply.github.com>
2024-08-21 23:41:31 -0700

    Ask before installing Code, Firefox & VLC

--- scripts/termux_scripts/xfce_termux-x11_install.sh
+++ scripts/termux_scripts/xfce_termux-x11_install.sh
@@ -4,7 +4,9 @@ yes | pkg upgrade
 echo "allow-external-apps = true" >> ~/.termux/termux.properties && termux-reload-settings
 pkg install x11-repo tur-repo && pkg install termux-x11-nightly xwayland -y
 pkg install xfce4 -y
-pkg install code-oss code-is-code-oss firefox vlc-qt -y
+echo -n "\nInstall Code , Firefox & VLC?\n[y/n]: "
+read i
+[ $i = "y" ] && { pkg install code-oss code-is-code-oss firefox vlc-qt -y ; }
 pkg install mesa-zink virglrenderer-mesa-zink vulkan-loader-android virglrenderer-android glmark2 -y
 
 rm -r $PREFIX/share/fonts/0xProto
